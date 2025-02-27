c1d610818f2ba32823e186a1a9deb1ba09001385
Glumling <rayyaan.haamid@gmail.com>
2025-02-27 00:52:43 -0600

    Initial commit

--- src/index.js
+++ src/index.js
@@ -4,13 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-
-
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-
-
 root.render(<App />);
